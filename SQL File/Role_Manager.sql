c4a3bc4c6f30d350fec74771d2287dd6811e385d
minhquanita01 <minhquan069.fit.hcmus@gmail.com>
2022-04-13 15:37:11 +0700

    Add SQL Script file to manage role in DB

---
+++ SQL File/Role_Manager.sql
@@ -0,0 +1,33 @@
+--Procedure xem quyền của role trên các đối tượng dữ liệu
+create or replace procedure View_Role_Privileges
+is
+begin
+    select ROLE_TAB_PRIVS.role as "Nhóm người dùng", table_name as "Tên bảng", column_name as "Tên cột", privilege "Quyền"
+    from ROLE_TAB_PRIVS join DBA_ROLES on ROLE_TAB_PRIVS.role = DBA_ROLES.role
+    where DBA_ROLES.oracle_maintained = 'N';
+end;
+/
+
+--Procedure tạo role tên là role_name
+create or replace procedure Create_Role(role_name in varchar2)
+is
+begin
+    execute immediate 'create role ' || role_name;
+end;
+/
+
+--Procudure xóa 1 role có tên role_name
+create or replace procedure Drop_Role(role_name in varchar2)
+is
+begin
+    execute immediate 'drop role ' || role_name;
+end;
+/
+
+--Procedure truất quyền priv_name trên đối tượng dữ liệu obj ra khỏi role_name
+create or replace procedure Revoke_Role_Privs(role_name in varchar2, priv_name in varchar2, obj in varchar2)
+is
+begin
+    execute immediate 'revoke ' || priv_name || ' on ' || obj || ' from ' || role_name;
+end;
+/
