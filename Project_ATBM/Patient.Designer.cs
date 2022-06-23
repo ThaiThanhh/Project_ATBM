0e550dcaee4313b944d4faa7b5f8fc25b8bda6f1
nqan1409 <quangtrungnguyen05021973@gmail.com>
2022-06-23 17:25:09 +0700

    Add view patient profile

---
+++ Project_ATBM/Patient.Designer.cs
@@ -0,0 +1,338 @@
+﻿namespace Project_ATBM
+{
+    partial class Patient
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Header = new System.Windows.Forms.Label();
+            this.labelMaBN = new System.Windows.Forms.Label();
+            this.textBoxMaBN = new System.Windows.Forms.TextBox();
+            this.textBoxMaCSYT = new System.Windows.Forms.TextBox();
+            this.labelMaCSYT = new System.Windows.Forms.Label();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.labelName = new System.Windows.Forms.Label();
+            this.textBoxID = new System.Windows.Forms.TextBox();
+            this.labelID = new System.Windows.Forms.Label();
+            this.textBoxDOB = new System.Windows.Forms.TextBox();
+            this.labelDOB = new System.Windows.Forms.Label();
+            this.textBoxNum = new System.Windows.Forms.TextBox();
+            this.labelNum = new System.Windows.Forms.Label();
+            this.textBoxStreet = new System.Windows.Forms.TextBox();
+            this.labelStreet = new System.Windows.Forms.Label();
+            this.textBoxDistrict = new System.Windows.Forms.TextBox();
+            this.labelDistrict = new System.Windows.Forms.Label();
+            this.textBoxCity = new System.Windows.Forms.TextBox();
+            this.labelCity = new System.Windows.Forms.Label();
+            this.textBoxMedicalHistory = new System.Windows.Forms.TextBox();
+            this.labelMedicalHistory = new System.Windows.Forms.Label();
+            this.textBoxFamilyMedicalHistory = new System.Windows.Forms.TextBox();
+            this.labelFamilyMedicalHistory = new System.Windows.Forms.Label();
+            this.textBoxAllergy = new System.Windows.Forms.TextBox();
+            this.labelAllergy = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Header
+            // 
+            this.Header.AutoSize = true;
+            this.Header.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Header.ForeColor = System.Drawing.Color.Red;
+            this.Header.Location = new System.Drawing.Point(384, 9);
+            this.Header.Name = "Header";
+            this.Header.Size = new System.Drawing.Size(108, 25);
+            this.Header.TabIndex = 12;
+            this.Header.Text = "Welcome ";
+            // 
+            // labelMaBN
+            // 
+            this.labelMaBN.AutoSize = true;
+            this.labelMaBN.Location = new System.Drawing.Point(143, 51);
+            this.labelMaBN.Name = "labelMaBN";
+            this.labelMaBN.Size = new System.Drawing.Size(91, 16);
+            this.labelMaBN.TabIndex = 13;
+            this.labelMaBN.Text = "Mã bệnh nhân";
+            // 
+            // textBoxMaBN
+            // 
+            this.textBoxMaBN.Location = new System.Drawing.Point(262, 48);
+            this.textBoxMaBN.Name = "textBoxMaBN";
+            this.textBoxMaBN.Size = new System.Drawing.Size(100, 22);
+            this.textBoxMaBN.TabIndex = 14;
+            // 
+            // textBoxMaCSYT
+            // 
+            this.textBoxMaCSYT.Location = new System.Drawing.Point(577, 45);
+            this.textBoxMaCSYT.Name = "textBoxMaCSYT";
+            this.textBoxMaCSYT.Size = new System.Drawing.Size(100, 22);
+            this.textBoxMaCSYT.TabIndex = 16;
+            // 
+            // labelMaCSYT
+            // 
+            this.labelMaCSYT.AutoSize = true;
+            this.labelMaCSYT.Location = new System.Drawing.Point(458, 48);
+            this.labelMaCSYT.Name = "labelMaCSYT";
+            this.labelMaCSYT.Size = new System.Drawing.Size(65, 16);
+            this.labelMaCSYT.TabIndex = 15;
+            this.labelMaCSYT.Text = "Mã CSYT";
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(252, 96);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(100, 22);
+            this.textBoxName.TabIndex = 18;
+            // 
+            // labelName
+            // 
+            this.labelName.AutoSize = true;
+            this.labelName.Location = new System.Drawing.Point(148, 99);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(96, 16);
+            this.labelName.TabIndex = 17;
+            this.labelName.Text = "Tên bệnh nhân";
+            // 
+            // textBoxID
+            // 
+            this.textBoxID.Location = new System.Drawing.Point(249, 155);
+            this.textBoxID.Name = "textBoxID";
+            this.textBoxID.Size = new System.Drawing.Size(100, 22);
+            this.textBoxID.TabIndex = 20;
+            // 
+            // labelID
+            // 
+            this.labelID.AutoSize = true;
+            this.labelID.Location = new System.Drawing.Point(152, 161);
+            this.labelID.Name = "labelID";
+            this.labelID.Size = new System.Drawing.Size(47, 16);
+            this.labelID.TabIndex = 19;
+            this.labelID.Text = "CMND";
+            // 
+            // textBoxDOB
+            // 
+            this.textBoxDOB.Location = new System.Drawing.Point(544, 155);
+            this.textBoxDOB.Name = "textBoxDOB";
+            this.textBoxDOB.Size = new System.Drawing.Size(100, 22);
+            this.textBoxDOB.TabIndex = 22;
+            // 
+            // labelDOB
+            // 
+            this.labelDOB.AutoSize = true;
+            this.labelDOB.Location = new System.Drawing.Point(403, 161);
+            this.labelDOB.Name = "labelDOB";
+            this.labelDOB.Size = new System.Drawing.Size(67, 16);
+            this.labelDOB.TabIndex = 21;
+            this.labelDOB.Text = "Ngày sinh";
+            // 
+            // textBoxNum
+            // 
+            this.textBoxNum.Location = new System.Drawing.Point(222, 216);
+            this.textBoxNum.Name = "textBoxNum";
+            this.textBoxNum.Size = new System.Drawing.Size(100, 22);
+            this.textBoxNum.TabIndex = 24;
+            // 
+            // labelNum
+            // 
+            this.labelNum.AutoSize = true;
+            this.labelNum.Location = new System.Drawing.Point(148, 219);
+            this.labelNum.Name = "labelNum";
+            this.labelNum.Size = new System.Drawing.Size(49, 16);
+            this.labelNum.TabIndex = 23;
+            this.labelNum.Text = "Số nhà";
+            // 
+            // textBoxStreet
+            // 
+            this.textBoxStreet.Location = new System.Drawing.Point(435, 222);
+            this.textBoxStreet.Name = "textBoxStreet";
+            this.textBoxStreet.Size = new System.Drawing.Size(100, 22);
+            this.textBoxStreet.TabIndex = 26;
+            // 
+            // labelStreet
+            // 
+            this.labelStreet.AutoSize = true;
+            this.labelStreet.Location = new System.Drawing.Point(349, 219);
+            this.labelStreet.Name = "labelStreet";
+            this.labelStreet.Size = new System.Drawing.Size(46, 16);
+            this.labelStreet.TabIndex = 25;
+            this.labelStreet.Text = "Đường";
+            // 
+            // textBoxDistrict
+            // 
+            this.textBoxDistrict.Location = new System.Drawing.Point(651, 222);
+            this.textBoxDistrict.Name = "textBoxDistrict";
+            this.textBoxDistrict.Size = new System.Drawing.Size(100, 22);
+            this.textBoxDistrict.TabIndex = 28;
+            // 
+            // labelDistrict
+            // 
+            this.labelDistrict.AutoSize = true;
+            this.labelDistrict.Location = new System.Drawing.Point(551, 225);
+            this.labelDistrict.Name = "labelDistrict";
+            this.labelDistrict.Size = new System.Drawing.Size(82, 16);
+            this.labelDistrict.TabIndex = 27;
+            this.labelDistrict.Text = "Quận/Huyện";
+            // 
+            // textBoxCity
+            // 
+            this.textBoxCity.Location = new System.Drawing.Point(871, 225);
+            this.textBoxCity.Name = "textBoxCity";
+            this.textBoxCity.Size = new System.Drawing.Size(100, 22);
+            this.textBoxCity.TabIndex = 30;
+            // 
+            // labelCity
+            // 
+            this.labelCity.AutoSize = true;
+            this.labelCity.Location = new System.Drawing.Point(786, 231);
+            this.labelCity.Name = "labelCity";
+            this.labelCity.Size = new System.Drawing.Size(101, 16);
+            this.labelCity.TabIndex = 29;
+            this.labelCity.Text = "Tỉnh/Thành phố";
+            // 
+            // textBoxMedicalHistory
+            // 
+            this.textBoxMedicalHistory.Location = new System.Drawing.Point(262, 279);
+            this.textBoxMedicalHistory.Name = "textBoxMedicalHistory";
+            this.textBoxMedicalHistory.Size = new System.Drawing.Size(100, 22);
+            this.textBoxMedicalHistory.TabIndex = 32;
+            // 
+            // labelMedicalHistory
+            // 
+            this.labelMedicalHistory.AutoSize = true;
+            this.labelMedicalHistory.Location = new System.Drawing.Point(144, 282);
+            this.labelMedicalHistory.Name = "labelMedicalHistory";
+            this.labelMedicalHistory.Size = new System.Drawing.Size(84, 16);
+            this.labelMedicalHistory.TabIndex = 31;
+            this.labelMedicalHistory.Text = "Tiền sử bệnh";
+            // 
+            // textBoxFamilyMedicalHistory
+            // 
+            this.textBoxFamilyMedicalHistory.Location = new System.Drawing.Point(259, 346);
+            this.textBoxFamilyMedicalHistory.Name = "textBoxFamilyMedicalHistory";
+            this.textBoxFamilyMedicalHistory.Size = new System.Drawing.Size(100, 22);
+            this.textBoxFamilyMedicalHistory.TabIndex = 34;
+            // 
+            // labelFamilyMedicalHistory
+            // 
+            this.labelFamilyMedicalHistory.AutoSize = true;
+            this.labelFamilyMedicalHistory.Location = new System.Drawing.Point(141, 349);
+            this.labelFamilyMedicalHistory.Name = "labelFamilyMedicalHistory";
+            this.labelFamilyMedicalHistory.Size = new System.Drawing.Size(134, 16);
+            this.labelFamilyMedicalHistory.TabIndex = 33;
+            this.labelFamilyMedicalHistory.Text = "Tiền sử bệnh gia đình";
+            // 
+            // textBoxAllergy
+            // 
+            this.textBoxAllergy.Location = new System.Drawing.Point(259, 394);
+            this.textBoxAllergy.Name = "textBoxAllergy";
+            this.textBoxAllergy.Size = new System.Drawing.Size(100, 22);
+            this.textBoxAllergy.TabIndex = 36;
+            // 
+            // labelAllergy
+            // 
+            this.labelAllergy.AutoSize = true;
+            this.labelAllergy.Location = new System.Drawing.Point(136, 400);
+            this.labelAllergy.Name = "labelAllergy";
+            this.labelAllergy.Size = new System.Drawing.Size(80, 16);
+            this.labelAllergy.TabIndex = 35;
+            this.labelAllergy.Text = "Dị ứng thuốc";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(513, 449);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 37;
+            this.button1.Text = "Cập nhật";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Patient
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1040, 506);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBoxAllergy);
+            this.Controls.Add(this.labelAllergy);
+            this.Controls.Add(this.textBoxFamilyMedicalHistory);
+            this.Controls.Add(this.labelFamilyMedicalHistory);
+            this.Controls.Add(this.textBoxMedicalHistory);
+            this.Controls.Add(this.labelMedicalHistory);
+            this.Controls.Add(this.textBoxCity);
+            this.Controls.Add(this.labelCity);
+            this.Controls.Add(this.textBoxDistrict);
+            this.Controls.Add(this.labelDistrict);
+            this.Controls.Add(this.textBoxStreet);
+            this.Controls.Add(this.labelStreet);
+            this.Controls.Add(this.textBoxNum);
+            this.Controls.Add(this.labelNum);
+            this.Controls.Add(this.textBoxDOB);
+            this.Controls.Add(this.labelDOB);
+            this.Controls.Add(this.textBoxID);
+            this.Controls.Add(this.labelID);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.labelName);
+            this.Controls.Add(this.textBoxMaCSYT);
+            this.Controls.Add(this.labelMaCSYT);
+            this.Controls.Add(this.textBoxMaBN);
+            this.Controls.Add(this.labelMaBN);
+            this.Controls.Add(this.Header);
+            this.Name = "Patient";
+            this.Text = "Patient";
+            this.Load += new System.EventHandler(this.Patient_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label Header;
+        private System.Windows.Forms.Label labelMaBN;
+        private System.Windows.Forms.TextBox textBoxMaBN;
+        private System.Windows.Forms.TextBox textBoxMaCSYT;
+        private System.Windows.Forms.Label labelMaCSYT;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.Label labelName;
+        private System.Windows.Forms.TextBox textBoxID;
+        private System.Windows.Forms.Label labelID;
+        private System.Windows.Forms.TextBox textBoxDOB;
+        private System.Windows.Forms.Label labelDOB;
+        private System.Windows.Forms.TextBox textBoxNum;
+        private System.Windows.Forms.Label labelNum;
+        private System.Windows.Forms.TextBox textBoxStreet;
+        private System.Windows.Forms.Label labelStreet;
+        private System.Windows.Forms.TextBox textBoxDistrict;
+        private System.Windows.Forms.Label labelDistrict;
+        private System.Windows.Forms.TextBox textBoxCity;
+        private System.Windows.Forms.Label labelCity;
+        private System.Windows.Forms.TextBox textBoxMedicalHistory;
+        private System.Windows.Forms.Label labelMedicalHistory;
+        private System.Windows.Forms.TextBox textBoxFamilyMedicalHistory;
+        private System.Windows.Forms.Label labelFamilyMedicalHistory;
+        private System.Windows.Forms.TextBox textBoxAllergy;
+        private System.Windows.Forms.Label labelAllergy;
+        private System.Windows.Forms.Button button1;
+    }
+}
